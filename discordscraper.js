3625cf89e63fe37c90be543b7881eb4594962158
Ben Hagle <ben.hagle@gmail.com>
2020-04-17 23:50:54 -0400

    Add discordscraper database upload, missing timed message selection

--- discordscraper.js
+++ discordscraper.js
@@ -6,6 +6,8 @@ const   Discord = require('discord.js'),
 
 const client = new Discord.Client();
 
+mongoose.connect("mongodb+srv://dbUser:2tZjrPcSr2VrwWx6@broumvirate-com-wvuvq.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, });
+
 var discordMessages = []
 var nickEntries = []
 
@@ -21,13 +23,33 @@ client.once('ready', () => {
 client.login('NzAwNDg1NDc2NDEwNDU4MTQy.XppVVQ.7Uh7yFPlV-FU0sdAw-yvW2AVbO8')
 
 function processNicks(mges){
-    var entry = {date:Date.now()}
     var changes = []
     for(i=0;i<mges.length;i++){
         mges.sort((a, b) => (a[0].timestamp, b[0].timestamp) ? -1 : 1)
         changes.push({  nickname:   mges[i][0].fields[1].value.slice(1, -1),
                         boy:        bmHelpers.discordTags[mges[i][0].author.name.split("#")[1]]})
     }
-    console.log(changes)
-    process.exit(0)
+
+    var entry = {date:Date.now(), nicknames:[]}
+    var idList = []
+    for(i=0;i<changes.length;i++){
+        if(idList.includes(changes[i].boy)){
+            //nickEntries.push(entry);
+            entry = {date:Date.now(), nicknames:[]}
+            idList = []
+        }
+        idList.push(changes[i].boy)
+        entry.nicknames.push(changes[i])
+    }
+    //nickEntries.push(entry);
+    Nick.insertMany(nickEntries, function(err, newNick){
+        if(err){
+            console.log(err);
+            process.exit(0);
+        }
+        else{
+            console.log(newNick);
+            process.exit(0);
+        }
+    })
 }
