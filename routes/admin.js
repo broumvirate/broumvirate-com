97097f4f6fcec0c4b2a0b5ddfa49d1215bd54b3f
Ben Hagle <ben.hagle@gmail.com>
2020-04-30 23:19:40 -0400

    laid groundwork for BAdmin panel

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,42 @@
-var express = require("express")
+var express = require("express");
 var router = express.Router();
-var bmHelpers = require("../bmHelpers")
+var bmHelpers = require("../bmHelpers");
 
 const   Boy = require("../models/boy"),
         User = require("../models/user");
 
 
 // ADMIN INDEX
-router.get("/admin", bmHelpers.isAdmin, function(req, res){
+router.get("/badmin", bmHelpers.isAdmin, function(req, res){
     Boy.find({}, function(err, Boys){
         if(err){
             console.log(err);
         }
         else{
-            User.find({}, function(err, Users){
+            User.find({}).populate("boy").exec(function(err, Users){
                 if(err){
                     console.log(err);
                 }
                 else{
-                    res.render("admin/admin", {pageName:"Admin Panel", Boys:Boys, Users:Users})
+                    res.render("admin/admin", {pageName:"Boy Admin Panel", Boys:Boys, Users:Users})
                 }
             })
         }
     })
 })
+
+// BOY New
+
+// BOY Edit
+
+// BOY Put
+
+// BOY Delete
+
+// USER Edit
+
+// USER Put
+
+// USER Delete
 
 module.exports = router;
