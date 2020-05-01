0a6c7431a65f9870c494bcecf7f4f4f35dfdf8f0
Ben Hagle <ben.hagle@gmail.com>
2020-05-01 16:16:31 -0400

    discordbot: Condensed message processing into one for loop, add comments

--- discord/discordscraper.js
+++ discord/discordscraper.js
@@ -6,7 +6,7 @@ const   Discord = require('discord.js'),
         bmHelpers = require("../bmHelpers")
         dotEnv = require("dotenv");
 
-const client = new Discord.Client();
+const   client = new Discord.Client();
 
 if (process.env.NODE_ENV !== 'production') {
     dotEnv.config({ path: '../.env' });
@@ -31,29 +31,37 @@ client.login(process.env.DISCORD)
 
 function processNicks(mges){
     var changes = []
-    mges.sort((a, b) => (a[0].timestamp, b[0].timestamp) ? -1 : 1)
-    for(i=0;i<mges.length;i++){
-        if(moment(mges[i][0].timestamp).isAfter(moment(Date.now()).subtract(1, "day"))){
-
-            changes.push({  nickname:   mges[i][0].fields[1].value.slice(1, -1),
-                            boy:        bmHelpers.discordTags[mges[i][0].author.name.split("#")[1]]})
-        }
-
-    }
-
-    var entry = {date:Date.now(), nicknames:[], dateString:moment(Date.now()).format("MMMM Do, YYYY")}
     var idList = []
-    for(i=0;i<changes.length;i++){
-        if(idList.includes(changes[i].boy)){
-            nickEntries.push(entry);
-            entry = {date:moment(Date.now()).add(i, "s").format(), nicknames:[], dateString:moment(Date.now()).format("MMMM Do, YYYY")}
-            idList = []
+    var entry = {date:Date.now(), nicknames:[], dateString:moment(Date.now()).format("MMMM Do, YYYY")}
+
+    mges.sort((a, b) => (a[0].timestamp, b[0].timestamp) ? -1 : 1)                              // Sort messages by timestamp
+    for(i=0;i<mges.length;i++){
+        if(moment(mges[i][0].timestamp).isAfter(moment(Date.now()).subtract(1, "day"))){        // For each message, if it's within the last day
+
+            let discordTag = mges[i][0].author.name.split("#")[1]                               // Grab the 4-digit discord tag from the user
+            if(Object.keys(bmHelpers.discordTags).includes(discordTag)){                        // If that user is in the discordTags:boys list, it is valid. Proceed.
+
+                let boyId = bmHelpers.discordTags[discordTag]                                   // Grab the boyId and the nickname
+                let thisNickname = mges[i][0].fields[1].value.slice(1, -1)
+
+                if(idList.includes(boyId)){                                                     // If we've already had that boy, push the entry to the finished entry list and reset it
+                    nickEntries.push(entry);                                                    // This code makes sure there are new entries if a boy comes up twice
+                    entry = {date:moment(Date.now()).add(i, "s").format(), nicknames:[], dateString:moment(Date.now()).format("MMMM Do, YYYY")}
+                    idList = []
+                }
+
+                idList.push(boyId)                                                              // Now, add this boy's ID to the list so we know when it comes up again
+                entry.nicknames.push({                                                          // Add the boy/nick pair to the entry list
+                   boy: boyId,
+                   nickname: thisNickname 
+                })
+
+            }
         }
-        idList.push(changes[i].boy)
-        entry.nicknames.push(changes[i])
     }
-    nickEntries.push(entry);
-    if(nickEntries[0].nicknames.length > 0){
+    nickEntries.push(entry);                                                                    // We're out of the loop, push the last entry to the nick entries list 
+
+    if(nickEntries[0].nicknames.length > 0){                                                    // Make sure there's at least one nickname change, and insert into database
         Nick.insertMany(nickEntries, function(err, newNick){
             if(err){
                 console.log(err);
