0ccd3798b9edb569af31608962dca40d16d813f4
Ben Hagle <ben.hagle@gmail.com>
2020-04-30 20:30:25 -0400

    Put nicknames behind login, also maybe broke nicknames

--- models/boy.js
+++ models/boy.js
@@ -3,10 +3,19 @@ const mongoose = require("mongoose")
 let boySchema = new mongoose.Schema({
 	name: String,
 	lastName: String,
+	email: String,
 	bid: Number,
-	active: Boolean,
-	registered: Boolean,
-	nickOrder: Number
+	active: Boolean,         //DELETE ME
+	registered: Boolean,     //DELETE ME
+	nickOrder: Number,       //order in which you're shown on nickname page
+	flags:{
+		active: Boolean,     //is active - ie. able to have a user account
+		coreBM: Boolean,     //is core broumvirate member
+		registered: Boolean, //is member registered
+		canRate: Boolean,    //allowed to have rating data entered
+		hasNicks: Boolean,   //nicknames are shown on nickname table
+		likesFrog: Boolean,  //does this man like frog?
+	}
 });
 
 module.exports = mongoose.model("boy", boySchema);
