deea22e707a8f96780be77dec5fcea2b170ccec4
Ben Hagle <ben.hagle@gmail.com>
2020-09-07 13:21:53 -0400

    Moved discord bot scraper to Tweetums

--- models/boy.js
+++ models/boy.js
@@ -1,22 +1,23 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 let boySchema = new mongoose.Schema({
-	name: String,
-	lastName: String,
-	email: String,
-	bid: Number,
-	active: Boolean,         //DELETE ME
-	registered: Boolean,     //DELETE ME
-	nickOrder: Number,       //order in which you're shown on nickname page
-	flags:{
-		isUser: Boolean,     //is a user - ie. able to have a user account
-		coreBm: Boolean,     //is core broumvirate member
-		registered: Boolean, //is member registered
-		canRate: Boolean,    //allowed to have rating data entered
-		hasNicks: Boolean,   //nicknames are shown on nickname table
-		likesFrog: Boolean,  //does this man like frog?
-		isAdmin: Boolean,
-	}
+    name: String,
+    lastName: String,
+    email: String,
+    bid: Number,
+    active: Boolean, //DELETE ME
+    registered: Boolean, //DELETE ME
+    nickOrder: Number, //order in which you're shown on nickname page
+    discordTag: String,
+    flags: {
+        isUser: Boolean, //is a user - ie. able to have a user account
+        coreBm: Boolean, //is core broumvirate member
+        registered: Boolean, //is member registered
+        canRate: Boolean, //allowed to have rating data entered
+        hasNicks: Boolean, //nicknames are shown on nickname table
+        likesFrog: Boolean, //does this man like frog?
+        isAdmin: Boolean,
+    },
 });
 
 module.exports = mongoose.model("boy", boySchema);
